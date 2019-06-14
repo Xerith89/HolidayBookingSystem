0eb4e0a99e5a0cfbc3d69d5057d7693c6f47e837
Alex Coupe <acoupe@gmail.com>
2019-06-14 21:36:53 +0100

    Login form redirects to dashboard - no auth yet

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,7 +12,8 @@
             <!-- Form -->
         
             <div class="form-group w-25 mx-auto text-center">
-                <form id="login-form" action="dashboard.php" method="POST">
+                <form id="login-form" action={{action('PagesController@Dashboard')}} method="POST">
+                    @csrf
                     <label>Username</label>
                     <input type="text" id="username" class="form-control" placeholder="Username" required>
                     <br>
