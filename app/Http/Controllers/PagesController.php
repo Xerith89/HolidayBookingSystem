5569e440c8335efc99de65fe4d83d1f70055227d
Alex Coupe <acoupe@gmail.com>
2019-06-20 18:01:09 +0100

    Going to index when auth will return to dashboard

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PagesController extends Controller
 {
     public function Index ()
     {
-        return view('pages.index');
+        if (Auth::check())
+        {
+            return redirect('dashboard');
+        } else {
+            return view('pages.index');
+        }
     }
 
     public function Dashboard ()
