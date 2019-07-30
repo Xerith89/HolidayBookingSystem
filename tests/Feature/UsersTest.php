88b854030a8dd398df7932a78e12d1b969c7a185
Alex Coupe <acoupe@gmail.com>
2019-07-30 19:33:34 +0100

    Some tests removed as they will be redesigned.

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -13,13 +13,13 @@ class UsersTest extends TestCase
    /** @test */
    public function not_logged_in_does_not_see_dashboard()
    {
-        $response = $this->get('/dashboard')->assertRedirect('/');
+        $this->get('/dashboard')->assertRedirect('/');
    }
 
    /** @test */
    public function logged_in_does_not_see_index()
    {
-        $response = $this->actingAs(factory(User::class)->create());
+        $this->actingAs(factory(User::class)->create());
 
         $response = $this->get('/')->assertRedirect('/dashboard');
    }
@@ -27,38 +27,10 @@ class UsersTest extends TestCase
    /** @test */
    public function logged_in_sees_dashboard()
    {
-        $response = $this->actingAs(factory(User::class)->create());
+        $this->actingAs(factory(User::class)->create());
 
-        $response = $this->get('/dashboard')->assertOk();
+        $this->get('/dashboard')->assertOk();
    }
 
-   /** @test */
-   public function user_can_be_added_through_form()
-   {
-        $response = $this->actingAs(factory(User::class)->create());
-
-        $response = $this->post('/dashboard/users', [
-        'name' => 'Alex Coupe',
-        'email' => 'test@test.com',
-        'staff_id' => 'AC001',        
-        'admin_user' => 'True',
-        'currentyear_holiday_entitlement' => '24.0',
-        'currentyear_holiday_used'  => '0',
-        'nextyear_holiday_entitlement'  => '24.0',
-        'nextyear_holiday_used'  => '0',
-        'pending_holiday_used'  => '0',
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ]);
-
-        $this->assertCount(1, User::all());
-   }
-
-   /** @test */
-   public function user_can_be_deleted_form()
-   {
-          $response = $this->actingAs(factory(User::class)->create());
-          $response = $this->delete(route('users.destroy', 1));
-          $this->assertEquals(302, $response->getStatusCode());
-   }
-
+   
 }
